5f128d5f4d532a1875a1fbc9b803cc65d80253b8
Marjorie <marjorie.gestin2020@campus-eni.fr>
2021-01-19 17:05:09 +0100

    build : DAL - fonction insertUser

--- src/fr/eni/encheres/models/dal/DAOFactory.java
+++ src/fr/eni/encheres/models/dal/DAOFactory.java
@@ -2,12 +2,19 @@ package fr.eni.encheres.models.dal;
 
 import fr.eni.encheres.models.dal.item.ItemDAO;
 import fr.eni.encheres.models.dal.item.ItemDAOJdbcImpl;
+import fr.eni.encheres.models.dal.user.UserDAO;
+import fr.eni.encheres.models.dal.user.UserDAOJdbcImpl;
 
 public class DAOFactory {
 
     public static ItemDAO getAvisDAO()
     {
         return new ItemDAOJdbcImpl();
+    }
+    
+    public static UserDAO getUserDAO() {
+    	
+    	return new UserDAOJdbcImpl();
     }
 
 }
