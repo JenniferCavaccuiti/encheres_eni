a4319c7d006474e84006e0922cf43f567af70c8c
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2021-01-20 06:14:12 +0100

    feat : display itemsList add css file

---
+++ src/fr/eni/encheres/models/bll/item/ItemManager.java
@@ -0,0 +1,24 @@
+package fr.eni.encheres.models.bll.item;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.models.bo.Item;
+import fr.eni.encheres.models.dal.DAOFactory;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemManager {
+
+    private static ItemManager instance = null;
+
+    public static ItemManager getInstance() {
+        if(instance == null) {
+            instance = new ItemManager();
+        }
+        return instance;
+    }
+
+    public List<Item> findAll() throws BusinessException, SQLException {
+       return DAOFactory.getItemDAO().findAll();
+    }
+}
--- src/fr/eni/encheres/models/dal/item/ItemDAO.java
+++ src/fr/eni/encheres/models/dal/item/ItemDAO.java
@@ -3,10 +3,11 @@ package fr.eni.encheres.models.dal.item;
 import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.models.bo.Item;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ItemDAO {
 
-    public List<Item> findAll() throws BusinessException;
+    public List<Item> findAll() throws BusinessException, SQLException;
 
 }
