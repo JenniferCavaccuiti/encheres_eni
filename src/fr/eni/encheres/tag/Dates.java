214a644a511e12529595c9f9dcee78c6936ccec9
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2021-01-20 09:41:00 +0100

    feat : add function formatdate for display

---
+++ src/fr/eni/encheres/tag/Dates.java
@@ -0,0 +1,14 @@
+package fr.eni.encheres.tag;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class Dates {
+    private Dates() {
+    }
+
+    public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+}
+
